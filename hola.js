6b5ab73fdfe94e8c74ab292da421b7cdbd5e89a6
YUTIX <143903761+CameronBustamanteLeveau2@users.noreply.github.com>
2023-09-30 06:50:46 +0000

    Update hola.js

--- hola.js
+++ hola.js
@@ -1,2 +1,10 @@
-console.log("hola mundo");
+console.log("Hello Yaakov");
+console.log("Good Bye John");
+console.log("Good Bye Jen");
+console.log("Hello Paul");
+console.log("Hello Frank");
+console.log("Hello Larry");
+console.log("Hello Paula");}
+console.log("Hello Laura");
+console.log("Good Bye Jim");
 console.log(`20 * 56 = ${20 * 56}`);
